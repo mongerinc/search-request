b0420a9f0b521a3f584d9a45fd58dd37e3df5261
Sergio <sdsomma@gmail.com>
2017-08-02 12:39:28 -0700

    add tests

---
+++ tests/Group/GroupTest.php
@@ -0,0 +1,65 @@
+<?php namespace Monger\SearchRequest\Tests\Group;
+
+use Monger\SearchRequest\SearchRequest;
+
+class GroupTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @test
+	 */
+	public function defaults()
+	{
+		$request = new SearchRequest;
+
+		$this->assertEquals([], $request->getGroups());
+	}
+
+	/**
+	 * @test
+	 */
+	public function singleString()
+	{
+		$request = new SearchRequest;
+
+		$request->groupBy('something');
+
+		$this->assertEquals(['something'], $request->getGroups());
+	}
+
+	/**
+	 * @test
+	 */
+	public function multipleStrings()
+	{
+		$request = new SearchRequest;
+
+		$request->groupBy('something')->groupBy('somethingElse');
+
+		$this->assertEquals(['something', 'somethingElse'], $request->getGroups());
+	}
+
+	/**
+	 * @test
+	 */
+	public function singleArray()
+	{
+		$request = new SearchRequest;
+
+		$request->groupBy(['something', 'somethingElse']);
+
+		$this->assertEquals(['something', 'somethingElse'], $request->getGroups());
+	}
+
+	/**
+	 * @test
+	 */
+	public function multipleArray()
+	{
+		$request = new SearchRequest;
+
+		$request->groupBy(['something'])->groupBy(['somethingElse']);
+
+		$this->assertEquals(['something', 'somethingElse'], $request->getGroups());
+	}
+
+}

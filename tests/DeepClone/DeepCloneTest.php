48a2127aa463a523b75cc422f3a394a0d85c22da
sergio <sdsomma@gmail.com>
2017-10-10 12:21:18 -0700

    add failing test

--- tests/DeepClone/DeepCloneTest.php
+++ tests/DeepClone/DeepCloneTest.php
@@ -9,12 +9,55 @@ class DeepCloneTest extends \PHPUnit_Framework_TestCase {
 	 */
 	public function deepClone()
 	{
-		$originalRequest = SearchRequest::create()->where('something', true);
+		$originalRequest = SearchRequest::create();
+
+		$originalRequest->page(2)->limit(10)
+		                ->term('something')
+		                ->select('something')
+		                ->where('something', true)
+		                ->addSort('something', 'asc')
+		                ->groupBy('something')
+		                ->facet('something')->page(2)->limit(100)->sortByCount()->setSortDirection('desc')->setMinimumCount(5)->includeOwnFilters();
+
 		$newRequest = clone $originalRequest;
 
-		$originalRequest->where('somethingElse', false);
+		$this->assertEquals($this->getExpectedJson(), $newRequest->toJson());
+	}
 
-		$this->assertNull($newRequest->getFilterValue('somethingElse'));
+	/**
+	 * Gets the expected json for the search request scenario
+	 *
+	 * @return string
+	 */
+	protected function getExpectedJson()
+	{
+		return json_encode([
+			'term' => 'something',
+			'page' => 2,
+			'limit' => 10,
+			'selects' => ['something'],
+			'groups' => ['something'],
+			'sorts' => [
+				['field' => 'something', 'direction' => 'asc'],
+			],
+			'filterSet' => [
+				'boolean' => 'and',
+				'filters' => [
+					['field' => 'something', 'operator' => '=', 'value' => true, 'boolean' => 'and'],
+				]
+			],
+			'facets' => [
+				[
+					'field' => 'something',
+					'sortType' => 'count',
+					'sortDirection' => 'desc',
+					'page' => 2,
+					'limit' => 100,
+					'minimumCount' => 5,
+					'excludesOwnFilters' => false,
+				]
+			],
+		]);
 	}
 
 }
